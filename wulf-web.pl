4db7765865f35bff901da44434e366d145cdb5e3
Adam DeConinck <adeconinck@rsystemsinc.com>
2011-09-01 14:13:34 -0500

    Many changes to views for vnfs, bootstrap, files

--- wulf-web.pl
+++ wulf-web.pl
@@ -13,6 +13,8 @@ use Warewulf::DataStore;
 use Warewulf::Provision::Pxelinux;
 use Warewulf::Provision::DhcpFactory;
 use Warewulf::Provision::HostsFile;
+use Warewulf::Util;
+use Digest::MD5 qw(md5_hex);
 
 # Use Template-Toolkit for template files.
 set 'template' => 'template_toolkit';
@@ -65,11 +67,26 @@ sub ww_avail_list {
 	return @list;
 }
 
+# Random string
+sub rand_string($) {
+	my $length = shift;
+	my @chars=('a'..'z');
+	my $rs;
+	foreach (1..$length) {
+		$rs.=$chars[rand @chars];
+	}
+	return $rs;
+}
+
 ###############################################################################
 # Begin route handlers.
 #
 # Index page: show a basic provisioning view.
 get '/' => sub {
+	forward '/node';
+};
+
+get '/node' => sub {
 	my $nodeSet = $db->get_objects("node","name",());
 	my %nodes;
 	foreach my $n ($nodeSet->get_list()) {
@@ -219,6 +236,269 @@ post '/node/:name' => sub {
 
 	template 'success.tt', {
 		'newaddr' => "/node/$name"
+	};
+
+};
+
+# Display vnfs list
+get '/vnfs' => sub {
+
+	my $vnfsSet = $db->get_objects('vnfs','name',());
+	my %vnfsList;
+	foreach my $vnfs ($vnfsSet->get_list()) {
+		$vnfsList{$vnfs->get('name')} = sprintf("%.1f",$vnfs->get('size')/(1024*1024));
+	}
+
+	template 'vnfs.tt', {
+		'vnfs' => \%vnfsList,
+	};
+
+};
+
+# Delete existing VNFS
+post '/vnfs/delete' => sub {
+
+	# Get list of VNFS's to delete.
+	# If >1 item, needs to be double-unrolled.
+	my @vnfsParams = params->{vnfs};
+	my @vnfsNames;
+	foreach my $item (@vnfsParams) {
+		if (ref($item) eq 'ARRAY') {
+			foreach my $subitem (@{$item}) {
+				push(@vnfsNames,$subitem);
+			}
+		} else {
+			push(@vnfsNames,$item);
+		}
+	}
+
+	# Get VNFS objects to be deleted.
+	my $vnfsSet = $db->get_objects('vnfs','name',@vnfsNames);
+	if ($vnfsSet->count() < @vnfsNames) {
+		print "Only found " . $vnfsSet->count() . " of the " . @vnfsNames . " VNFS's\n";
+		return;	
+	}
+	$db->del_object($vnfsSet);
+
+	template 'success.tt', {
+		'newaddr'=>"/vnfs",
+	};
+};
+
+# TODO: Complete VNFS upload option.
+post '/vnfs/upload' => sub {
+
+	my $upload = upload('vnfs');
+	my $basename = $upload->basename();
+
+};
+
+# Display bootstrap list
+get '/bootstrap' => sub {
+
+	my $bootstrapSet = $db->get_objects('bootstrap','name',());
+	my %bootstrapList;
+	foreach my $bootstrap ($bootstrapSet->get_list()) {
+		$bootstrapList{$bootstrap->get('name')} = sprintf("%.1f",$bootstrap->get('size')/(1024*1024));
+	}
+
+	template 'bootstrap.tt', {
+		'bootstrap' => \%bootstrapList,
+	};
+
+};
+
+# Delete existing bootstrap
+post '/bootstrap/delete' => sub {
+
+	# Get list of VNFS's to delete.
+	# If >1 item, needs to be double-unrolled.
+	my @bootstrapParams = params->{bootstrap};
+	my @bootstrapNames;
+	foreach my $item (@bootstrapParams) {
+		if (ref($item) eq 'ARRAY') {
+			foreach my $subitem (@{$item}) {
+				push(@bootstrapNames,$subitem);
+			}
+		} else {
+			push(@bootstrapNames,$item);
+		}
+	}
+
+	# Get VNFS objects to be deleted.
+	my $bootstrapSet = $db->get_objects('bootstrap','name',@bootstrapNames);
+	if ($bootstrapSet->count() < @bootstrapNames) {
+		print "Only found " . $bootstrapSet->count() . " of the " . @bootstrapNames . " VNFS's\n";
+		return;	
+	}
+	$db->del_object($bootstrapSet);
+
+	template 'success.tt', {
+		'newaddr'=>"/bootstrap",
+	};
+};
+
+# Display file list
+get '/file' => sub {
+
+	my $fileSet = $db->get_objects('file','name',());
+	my %fileList;
+	foreach my $file ($fileSet->get_list()) {
+		$fileList{$file->get('name')} = sprintf("%f",$file->get('size'));
+	}
+
+	template 'file.tt', {
+		'file' => \%fileList,
+	};
+
+};
+
+# Delete existing file
+post '/file/delete' => sub {
+
+	# Get list of VNFS's to delete.
+	# If >1 item, needs to be double-unrolled.
+	my @fileParams = params->{file};
+	my @fileNames;
+	foreach my $item (@fileParams) {
+		if (ref($item) eq 'ARRAY') {
+			foreach my $subitem (@{$item}) {
+				push(@fileNames,$subitem);
+			}
+		} else {
+			push(@fileNames,$item);
+		}
+	}
+
+	# Get VNFS objects to be deleted.
+	my $fileSet = $db->get_objects('file','name',@fileNames);
+	if ($fileSet->count() < @fileNames) {
+		print "Only found " . $fileSet->count() . " of the " . @fileNames . " VNFS's\n";
+		return;	
+	}
+	$db->del_object($fileSet);
+
+	template 'success.tt', {
+		'newaddr'=>"/file",
+	};
+};
+
+# Show file information
+get '/file/:name' => sub {
+	my $name = params->{name};
+	my $fileSet = $db->get_objects('file','name',($name));
+	if ($fileSet->count() == 0) {
+		print "No file $name found!\n";
+	}
+	my $file = $fileSet->get_object(0);
+	my $uid = $file->get('uid');
+	my $gid = $file->get('gid');
+	my $path = $file->get('path');
+	my $mode = $file->get('mode');
+	my $id = $file->get('_id');
+
+	# Get file contents
+	my $contents = "";
+	my $binstore = $db->binstore($file->get('_id'));
+	while (my $buffer = $binstore->get_chunk()) {
+		$contents = $contents . $buffer;
+	}
+
+	template 'fileview.tt', {
+		'name' => $name,
+		'id' => $id,
+		'uid' => $uid,
+		'gid' => $gid,
+		'path' => $path,
+		'mode' => $mode,
+		'contents' => $contents,
+	};
+
+};
+
+# Set file information
+post '/file/:name' => sub {
+	my $name = params->{name};
+	my $id = params->{id};
+	my $uid = params->{uid};
+	my $gid = params->{gid};
+	my $path = params->{path};
+	my $mode = params->{mode};
+	my $contents = params->{contents};
+	my $persist = params->{persist};
+
+	# Start checking for validity
+	my $invalid = "";
+	if (not ($path =~ /^([a-zA-Z0-9\-_\/\.]+)$/)) {
+		$invalid = $invalid . " path ";
+	}
+	if (not ($uid =~ /^[0-9]+$/) ) {
+		$invalid = $invalid . " uid ";
+	}
+	if (not ($gid =~ /^[0-9]+$/) ) {
+		$invalid = $invalid . " gid ";
+	}
+	if (not ($name =~ /^[a-zA-Z0-9\-_\.]+$/)) {
+		$invalid = $invalid . " name ";
+	}
+	if (not ($mode =~ /^[0-7]{3,4}$/)) {
+		$invalid = $invalid . " mode ";
+	}
+	if ($invalid ne "") {
+		print "Can't save! Invalid " . $invalid;
+		return;
+	}
+
+	# Get file object
+        my $fileSet = $db->get_objects('file','_id',($id));
+        if ($fileSet->count() == 0) {
+                print "No file $name found!\n";
+        }
+        my $file = $fileSet->get_object(0);
+	
+	# Check if contents changed; if so, change them.
+	if ($persist) {
+		my $rand = rand_string(16);
+        	my $tmpfile = "/tmp/wwsh.$rand";
+	        my $digest1;
+        	my $digest2;
+		open(TMPFILE,">$tmpfile") or die "File error!\n";
+		print TMPFILE $contents;
+		close(TMPFILE);
+		$digest1 = $file->get('checksum');
+		$digest2 = digest_file_hex_md5($tmpfile);
+		my $size = 0;
+
+		if ($digest1 ne $digest2) {
+		    my $buffer;
+                    open(FILE, $tmpfile);
+		    my $binstore = $db->binstore($file->get("_id"));
+                    while(my $length = sysread(FILE, $buffer, 15*1024*1024)) {
+			$binstore->put_chunk($buffer);
+#                        if (! $size) {
+#                            $file->set("format", &format($buffer));
+#                        }
+                        $size += $length;
+                    }
+		    close(FILE);
+		    $file->set('checksum',$digest2);
+		    $file->set('size',$size);
+		    
+		}
+	}
+
+	# Save metadata
+	$file->set('name',$name);
+	$file->set('uid',$uid);
+	$file->set('gid',$gid);
+	$file->set('path',$path);
+	$file->set('mode',$mode);
+
+
+	$db->persist($fileSet);
+
+	template 'success.tt', {
+		'newaddr' => "/file/$name",
 	};
 
 };
