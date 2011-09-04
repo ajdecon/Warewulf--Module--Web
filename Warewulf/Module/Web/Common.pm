bf552b76e9351c4b91a2062d71bc045b3b358b96
Adam DeConinck <ajdecon@ajdecon.org>
2011-09-03 19:35:27 -0500

    Created a couple of common functions

--- Warewulf/Module/Web/Common.pm
+++ Warewulf/Module/Web/Common.pm
@@ -1,10 +1,31 @@
 #!/usr/bin/env perl
 
-use Dancer;
-use Template;
 use Warewulf::DataStore;
 use Warewulf::Util;
 
-set 'template' => 'template_toolkit';
-
 my $db = Warewulf::DataStore->new();
+
+my @valid_types = ('vnfs', 'file', 'node', 'bootstrap');
+
+sub get_object {
+
+    my $type = shift;
+    my $name = shift;
+
+    my $object_set = $db->get_objects($type,'name',($name));
+    if ($object_set->count() == 0) {
+        print "$type $name not found!\n;";
+        return;
+    } elsif ($object_set->count() > 1) {
+        print "$name not unique in $type !\n";
+    }
+
+    my $object = $object_set->get_object(0);
+    return $object;
+
+};
+
+sub get_full_list {
+    my $type = shift;
+    return ($db->get_objects($type,'name',()))->get_list();
+}
