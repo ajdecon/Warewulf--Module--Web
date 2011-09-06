7e3e3dcf928a40a05333e73c000f2c4ea4e109e0
Adam DeConinck <adeconinck@rsystemsinc.com>
2011-09-06 15:07:21 -0500

    Added some very basic authentication functionality

--- wulf-dance.pl
+++ wulf-dance.pl
@@ -1,6 +1,4 @@
 #!/usr/bin/env perl
-#
-
 # package Warewulf::Module::Web;
 
 use Dancer;
@@ -11,6 +9,33 @@ use WWWeb::Bootstrap;
 use WWWeb::File;
 
 prefix undef;
+set session => 'Simple';
+
+before sub {
+
+    if (! session('user') && request->path_info !~ m{^/login}) {
+         var requested_path => request->path_info;
+        request->path_info('/login');
+    }
+};
+
+get '/login' => sub {
+	template 'login.tt', { path => vars->{requested_path} };
+};
+
+post '/login' => sub {
+	if (params->{user} eq 'admin' && params->{pass} eq 'wulf') {
+		session user => params->{user};
+		redirect params->{path} || '/';
+	} else {
+		redirect '/login?failed=1';
+	}
+};
+
+get '/logout' => sub {
+	session->destroy;
+	redirect '/';
+};
 
 get '/' => sub {
 	forward('/node/all');
